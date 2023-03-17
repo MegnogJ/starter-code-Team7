b6663e2c9edcd965dd81d1709fbcb9d90bd74c19
shibaynu <118879128+shibaynu@users.noreply.github.com>
2023-03-17 13:17:40 -0400

    Added name in main

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -5,6 +5,8 @@ public class Main {
   public static void main(String[] args) {
     App.launch(App.class, args);
 
+    System.out.println("Aaron Zhang");
+
     System.out.println("Mahir Sowad");
 
     System.out.println("Zimraan Ahmad");
